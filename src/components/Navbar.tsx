dbb20d1dd7ec62ace192f06b58e0610a80df8c31
vaidik <vaidiksinghnirwan@gmail.com>
2024-08-25 17:43:05 +0530

    developed the hero section and made changes in the navbar section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import { navLists } from '../constants'
 const Navbar = () => {
   return (
     <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
-        <nav className='flex w-full screen-max-width'>
+        <nav className='flex w-full justify-between screen-max-width'>
             <img src={appleImg} alt="appleImg" width={14} height={18} />
             <div className='flex flex-1 justify-center max-sm:hidden'>
                 {navLists.map((nav,i) => (
